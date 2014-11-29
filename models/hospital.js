beb62b75936c5d47c4453e5dc533c8fa9d495c05
ErythroME <megas.nmsis@hotmail.com>
2014-11-29 23:51:36 +0800

    怨念2：添加hospital模型

---
+++ models/hospital.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Megas on 2014/11/29.
+ * 医呼百应 医院模型
+ * 根据14-05-数据库设计说明书v1.1.0完成
+ * Todo：未加索引
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+var config = require('../config');
+
+var HospitalSchema = new Schema({
+    hospital_id: { type: ObjectId },
+    hospital_name: { type: String },
+    hospital_intro: { type: String },
+    hospital_is_validated: { type: Boolean, default: false },
+    hospital_dept: [
+        {
+            hospital_dept_id: { type: ObjectId },
+            hospital_dept_name: { type: String },
+            hospital_dept_doc: [{ type: ObjectId }]
+        }
+    ]
+});
+
+mongoose.model('Hospital', HospitalSchema);
