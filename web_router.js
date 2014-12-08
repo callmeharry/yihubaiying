75c8e74248a556eae74e7c188b6bb760e04cc337
lewiskit <lewisfyh2012@gmail.com>
2014-12-08 19:28:15 +0800

    add admin login page

--- web_router.js
+++ web_router.js
@@ -2,12 +2,11 @@
  * Created by steve on 14-12-1.
  * 医呼百应:路由控制
  */
-
-
 var express = require('express');
 var smsTest = require('./test/sms_test');
 var getpost = require('./test/getpost');
 var mobileRegister = require('./controllers/register');
+var admin = require('./controllers/admin');
 var router = express.Router();
 
 
@@ -15,9 +14,9 @@ var router = express.Router();
 var index = require('./test/index');
 var user = require('./test/users');
 
+
 //Home page
 router.get('/', index);
-router.get('/', user);
 
 //Mobile register page
 router.get('/mobile_register', mobileRegister.showRegister);
@@ -27,6 +26,14 @@ router.post('/mobile_register', mobileRegister.doRegister);
 //Test page
 router.get('/sms-test', smsTest.registerVerity);
 router.post('/pay',getpost.do);
+
+/**
+ * Administrator pages
+ */
+
+router.get('/admin_login', admin.showAdminLogin);
+router.post('/admin_login', admin.adminLogin);
+router.get('/admin/index', admin.adminIndex);
 
 
 module.exports = router;
