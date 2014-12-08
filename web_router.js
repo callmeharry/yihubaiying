875962e018a4595bd7d323e1d53e3421c733a5a0
callmeharry <cmhsteveli@gmail.com>
2014-12-08 19:46:34 +0800

    hospital&departments select 80% complete

--- web_router.js
+++ web_router.js
@@ -5,9 +5,11 @@
 var express = require('express');
 var smsTest = require('./test/sms_test');
 var getpost = require('./test/getpost');
+var mobileBook = require('./controllers/book');
 var mobileRegister = require('./controllers/register');
 var admin = require('./controllers/admin');
 var mobileLogin = require('./controllers/login');
+var mobileIndex = require('./controllers/site');
 var router = express.Router();
 
 
@@ -18,18 +20,26 @@ var user = require('./test/users');
 //Home page
 router.get('/', index);
 
+//Mobile home page
+router.get('/mobile', mobileIndex.showIndex);//show mobile index page
+
 //Mobile register page
-router.get('/mobile_register', mobileRegister.showRegister);
-router.post('/mobile_generate_auth_code', mobileRegister.generateAuthCode);
-router.post('/mobile_register', mobileRegister.doRegister);
+router.get('/mobile/register', mobileRegister.showRegister);//show mobile register page
+router.post('/mobile/generateAuthCode', mobileRegister.generateAuthCode);//generate a 6-digit auth code
+router.post('/mobile/register', mobileRegister.handleRegister);//handle register info
 
 //Mobile login page
 router.get('/mobile_login', mobileLogin.showLogin);
 router.post('/mobile_login', mobileLogin.handleLogin);
 
+//Book page
+router.get('/mobile/book/hospitals', mobileBook.showHospital);
+router.get('/mobile/book/hospitals/departments', mobileBook.showDepartment);
+
 //Test page
 router.get('/sms-test', smsTest.registerVerity);
 router.post('/pay',getpost.do);
+
 
 /**
  * Administrator pages
