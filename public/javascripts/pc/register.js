9cabc41a899c0754be52f9c1da5769774311b875
jadegong <jadegong@sina.cn>
2014-12-14 21:36:32 +0800

    修改注册页面
    修改注册页面与手机端的名称一致！并增加相应的响应！
    By 龚琦丹2014-12-14

--- public/javascripts/pc/register.js
+++ public/javascripts/pc/register.js
@@ -1,27 +1,77 @@
 function checkAgree ()
 {
 	// body...
-	if (document.getElementById('register-Agree').checked == true)
+	if (document.getElementById('register_Agree').checked == true)
 	{
-		document.getElementById('register-submit').disabled = "";
+		document.getElementById('register_submit').disabled = "";
 	}
 	else
 	{
-		document.getElementById('register-submit').disabled = "disabled";
+		document.getElementById('register_submit').disabled = "disabled";
 	}
 }
 function checkSubmit ()
 {
 	// body...
-	var password = document.getElementById('register-Password').value;
-	var confirmPassword = document.getElementById('register-confirmPassword').value;
-	if (password == confirmPassword)
+	var result_PasswordLength = showPasswordLengthWarning();
+	var result_RepeatPassword = showRepeatPasswordWarning();
+	var result_Phone = showPhoneWarning();
+	if (result_Phone&&result_RepeatPassword&&result_PasswordLength)
 	{
-		return true;
+	 	return true;
 	}
 	else
 	{
-		alert("两次密码不一样，请重新输入！");
+	 	alert ("请先修改错误，然后再提交注册！");
 		return false;
 	}
+}
+function showPasswordLengthWarning()
+{
+    element = document.getElementById('id_passWord').value;
+    if (element.length > 20 || element.length < 6)
+    {
+        //alert(document.getElementById("id_warning").innerHTML);
+        document.getElementById('id_warning').style.display = "";
+        return false;
+    }
+    else
+    {
+        document.getElementById('id_warning').style.display = "none";
+        return true;
+    }
+}
+function showRepeatPasswordWarning()
+{
+    element = document.getElementById('id_repeat_passWord').value;
+    element1 = document.getElementById('id_passWord').value;
+
+    if (element != element1)
+    {
+        //alert(document.getElementById("id_warning").innerHTML);
+        document.getElementById('id_repeat_warning').style.display = "";
+        return false;
+    }
+    else
+    {
+        document.getElementById('id_repeat_warning').style.display = "none";
+        return true;
+    }
+}
+function showPhoneWarning()
+{
+    element = document.getElementById('id_phoneNumber').value;
+    l = element.length;
+
+    if (l != 11)
+    {
+        //alert(document.getElementById("id_warning").innerHTML);
+        document.getElementById('id_phone_warning').style.display = "";
+        return false;
+    }
+    else
+    {
+        document.getElementById('id_phone_warning').style.display = "none";
+        return true;
+    }
 }
