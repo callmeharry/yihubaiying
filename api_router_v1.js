43812f4dbe6d14eea03cfdd9c17e9feec0b3763a
ErythroME <megas.nmsis@hotmail.com>
2014-12-19 13:07:30 +0800

    修改api_router

--- api_router_v1.js
+++ api_router_v1.js
@@ -6,9 +6,15 @@
 var express = require('express');
 var router = express.Router();
 
-var showHospitalInfoController = require('./api/v1/hospital');
-var updateHospitalInfoController = require('./api.v1/hospital');
+var hospitalController = require('./api/v1/hospital');
+var orderController = require('./api/v1/order');
 
-router.get();
+router.get('/hospital/:id', hospitalController.showHospitalInfo);
+router.post('/hospital/:id/update', hospitalController.updateHospitalBicInfo);
+router.post('/hospital/:id/dept_id', hospitalController.updateHospitalDeptInfo);
+router.post('hospital/:id/remove', hospitalController.removeHospitalInfo);
+router.post('hospital/:id/dept_id/doc_id', hospitalController.removeDocFromDept);
+router.post('hospital/:id/dept_id/doc_id/add', hospitalController.addDocToDept);
+router.get('/orders', orderController.getOrderInfo);
 
 module.exports = router;
