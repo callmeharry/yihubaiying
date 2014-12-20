b2caea8738ec05075f48eab80dc44eaee3bbad19
lewiskit <lewisfyh2012@gmail.com>
2014-12-20 19:47:20 +0800

    modify userinfo

--- controllers/user.js
+++ controllers/user.js
@@ -13,7 +13,7 @@ var validator = require('validator');
 exports.showPersonInfo = function (req, res, next) {
     var user = req.session.user;
 
-  
+
 
     User.getUserById(user._id, function (err,user) {
         if(err) return next(err);
@@ -54,31 +54,31 @@ exports.changeCity = function (req, res, next) {
 };
 
 exports.changePhoneNumber = function (req, res, next) {
-    var user_id = res.cookies.user_id;
+    var current_user = req.session.user;
     var newPhoneNumber = req.body.newPhoneNumber || '';
 
     if (newPhoneNumber !== '')
         return res.send({status: -1, msg: "failed"});
 
-    User.getUserById(user_id, function (user) {
+    User.getUserById(current_user._id, function (user) {
         user.phone_number = newPhoneNumber;
         user.save(function (err) {
             if (err) return res.send({status: -1, msg: "failed"});
 
-            return res.rend({status: 0});
+            return res.send({status: 0});
         });
     });
 
 };
 
 exports.changeEmail = function (req, res, next) {
-    var user_id = res.cookies.user_id;
-    var newEmail = validator(req.body.newEmail);
+    var user = req.session.user;
+    var newEmail = validator(req.body.newEmail)||'';
 
     if (newEmail !== '')
         res.send({status: 0, msg: "can not be null"});
 
-    User.getUserById(user_id, function (user) {
+    User.getUserById(user._id, function (user) {
         user.email = newEmail;
         user.save(function (err) {
             if (err) return res.send({status: 0, msg: "can not be null"});
